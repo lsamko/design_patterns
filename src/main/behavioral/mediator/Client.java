d503e27d093e86132012866af25f34ec22b38f41
Liudmyla Topchii <liuda.samko@gmail.com>
2024-01-29 15:01:14 +0200

    added mediator pattern

---
+++ src/main/behavioral/mediator/Client.java
@@ -0,0 +1,18 @@
+package main.behavioral.mediator;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Mediator mediator = new Mediator();
+
+        ChatUser alice = new ChatUser("Alice", mediator);
+        ChatUser bob = new ChatUser("Bob", mediator);
+        ChatUser carol = new ChatUser("Carol", mediator);
+
+        mediator.addUser(alice).addUser(bob).addUser(carol);
+
+        carol.sendMessage("Hello everyone");
+
+    }
+
+}
